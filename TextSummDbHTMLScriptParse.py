d732fbade8997124bf5bb39145f2e622a947febc
Md Tahsin Asif <32189971+mdtahsinasif@users.noreply.github.com>
2020-02-02 00:37:41 +0530

    Update TextSummDbHTMLScriptParse.py
    Updated File

--- TextSummDbHTMLScriptParse.py
+++ TextSummDbHTMLScriptParse.py
@@ -4,61 +4,64 @@ from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 from nltk.tokenize import word_tokenize, sent_tokenize
 from bs4 import BeautifulSoup
+from pymongo import MongoClient
 import re
 import pandas as pd
 import os
 import datetime
 import json
-
-from pymongo import MongoClient
-#conda install -c anaconda beautifulsoup4
- 
-
-
-
-#text_str = open(r"C:/Users/tahsin.asif/OneDrive - Antuit India Private Limited/Asif/AI/TextSummarization/text.txt","r")
-from pathlib import Path
-text_str1 = Path('C:/Users/tahsin.asif/OneDrive - Antuit India Private Limited/Asif/AI/TextSummarization/text.txt').read_text(encoding='utf-8')
-
-
 import urllib.request
-# open a connection to a URL using urllib
-#webUrl  = urllib.request.urlopen('https://ciso.economictimes.indiatimes.com/news/user-data-not-affected-from-new-mp4-file-bug-whatsapp/72106154?utm_source=RSS&utm_medium=ETRSS')
 
-#get the result code and print it
-#print ("result code: " + str(webUrl.getcode()))
+
+
+
+# text_str = open(r"C:/Users/tahsin.asif/OneDrive - Antuit India Private Limited/Asif/AI/TextSummarization/text.txt","r")
+from pathlib import Path
+
+text_str1 = Path(
+    'C:/Users/tahsin.asif/OneDrive - Antuit India Private Limited/Asif/AI/TextSummarization/text.txt').read_text(
+    encoding='utf-8')
+
+
+# open a connection to a URL using urllib
+# webUrl  = urllib.request.urlopen('https://ciso.economictimes.indiatimes.com/news/user-data-not-affected-from-new-mp4-file-bug-whatsapp/72106154?utm_source=RSS&utm_medium=ETRSS')
+
+# get the result code and print it
+# print ("result code: " + str(webUrl.getcode()))
 
 ####################
 
 ####################
 # read the data from the URL and print it
-#text_str = webUrl.read()
-#print ('text_str2----------->',text_str)
+# text_str = webUrl.read()
+# print ('text_str2----------->',text_str)
 
 #####################
-#url = 'https://ciso.economictimes.indiatimes.com/news/user-data-not-affected-from-new-mp4-file-bug-whatsapp/72106154?utm_source=RSS&utm_medium=ETRSS'
+# url = 'https://ciso.economictimes.indiatimes.com/news/user-data-not-affected-from-new-mp4-file-bug-whatsapp/72106154?utm_source=RSS&utm_medium=ETRSS'
 
-#headers = {}
-#headers['User-Agent'] = 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17'
+# headers = {}
+# headers['User-Agent'] = 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17'
 
-#request = urllib.request.Request(url, headers=headers)
-#text_str3 = urllib.request.urlopen(request)
+# request = urllib.request.Request(url, headers=headers)
+# text_str3 = urllib.request.urlopen(request)
 
-#print(text_str3.read())
+# print(text_str3.read())
 #####################
 
 from pathlib import Path
-text_str1 = Path('C:/Users/tahsin.asif/OneDrive - Antuit India Private Limited/Asif/AI/TextSummarization/text.txt').read_text(encoding='utf-8')
 
+text_str1 = Path(
+    'C:/Users/tahsin.asif/OneDrive - Antuit India Private Limited/Asif/AI/TextSummarization/text.txt').read_text(
+    encoding='utf-8')
 
 text_str1 = '''
 '''
 
-
 url = 'https://ciso.economictimes.indiatimes.com/news/user-data-not-affected-from-new-mp4-file-bug-whatsapp/72106154?utm_source=RSS&utm_medium=ETRSS'
 
 headers = {}
-headers['User-Agent'] = 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17'
+headers[
+    'User-Agent'] = 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17'
 
 request = urllib.request.Request(url, headers=headers)
 text_str2 = urllib.request.urlopen(request)
@@ -75,9 +78,9 @@ def _create_frequency_table(text_string) -> dict:
     :rtype: dict
     """
     stopWords = set(stopwords.words("english"))
-    # adding beautiful soap logic to get clean text from html 
-    
-    words = word_tokenize(review_text)
+    # adding beautiful soap logic to get clean text from html
+
+    words = word_tokenize(text_string)
     ps = PorterStemmer()
 
     freqTable = dict()
@@ -119,7 +122,7 @@ def _score_sentences(sentences, freqTable) -> dict:
         '''
         Notice that a potential issue with our score algorithm is that long sentences will have an advantage over short sentences. 
         To solve this, we're dividing every sentence score by the number of words in the sentence.
-        
+
         Note that here sentence[:10] is the first 10 character of any sentence, this is to save memory while saving keys of
         the dictionary.
         '''
@@ -154,15 +157,11 @@ def _generate_summary(sentences, sentenceValue, threshold):
     return summary
 
 
-
-
 def run_summarization(text):
-    
-    
-   # print('Inside Review Text')
+    #print('Inside Review Text',text)
     # 1 Create the word frequency table
     freq_table = _create_frequency_table(text)
-
+    #print('Inside freq_table------------',)
     '''
     We already have a sentence tokenizer, so we just need 
     to run the sent_tokenize() method to create the array of sentences.
@@ -178,48 +177,58 @@ def run_summarization(text):
     threshold = _find_average_score(sentence_scores)
 
     # 5 Important Algorithm: Generate the summary
-    summary = _generate_summary(sentences, sentence_scores,.5 * threshold)
-
+    summary = _generate_summary(sentences, sentence_scores, .75 * threshold)
+    print("After Summary")
     return summary
 
-def run_dbConnection(localhost,port):
+
+def run_dbConnection(localhost, port):
     connection = MongoClient()
     connection = MongoClient('localhost', 27017)
     db = connection.core
     collection = db.rss_feed_entry
-    mydoc = collection.find({},{ "_id": 0,"description": 1 ,
-                        "feedUrl":1,
-                        "link":1,
-                        "date": datetime.datetime.utcnow()
-                        }).limit(3)
+    mydoc = collection.find({}, {"_id": 0, "description": 1,
+                                 "feedUrl": 1,
+                                 "link": 1,
+                                 "date": datetime.datetime.utcnow()
+                                 }).limit(1)
     for x in mydoc:
         jsonToStr = json.dumps(x)
         count = len(jsonToStr)
         list_key = x["link"]
         list_val = x["description"]
-        lenList = len(list_key)
-      #  print('Success-------->',lenList)
-        if lenList < 60:
-           # print(review_text)
+        lenList = len(list_val)
+        print('Success-------->',lenList)
+        if lenList < 190:
+            print('condition true---')
+            headers = {}
+            headers[
+                'User-Agent'] = 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17'
+
+            request = urllib.request.Request(list_key, headers=headers)
+            text_str2 = urllib.request.urlopen(request)
+            print("text_str2-------------------------")
+
+            text_str = text_str2.read()
+            # print('review_text------->',text_str)
+            # 1.Remove HTML
+            soup = BeautifulSoup(text_str, "html.parser")
+            print("After SOup========================")
+            [x.extract() for x in soup.findAll(['script', 'style'])]
+            review_text = soup.get_text(strip=True)
+            print("After review_text========================")
             result = run_summarization(review_text)
-            #print('condition true---',result)
-            
-     
+            print(result)
+            # print(review_text)
+
+
+
 
 
 if __name__ == '__main__':
-    
     text_str1 = ''
-    host ='localhost'
+    host = 'localhost'
     port = 27017
-    run_dbConnection(host,port)
-    #print('review_text------->',text_str)
-    #1.Remove HTML
-    soup = BeautifulSoup(text_str, "html5lib")
-    [x.extract() for x in soup.findAll(['script', 'style'])]
-   # review_text = BeautifulSoup(soup).get_text()
-   # print('review_text------->',soup.prettify())
-   # soup.find('div',class_='msg-yellow').decompose()
-    
-    result = run_summarization(soup.get_text())
-    print(result)
+    run_dbConnection(host, port)
+
+
